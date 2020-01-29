b42cba6b0c056b127489f9e1586a83d8963e6668
wstygg <wstygg@student.21-school.ru>
2020-01-29 17:31:15 +0300

    hash dir

---
+++ hash_table/cmake-build-debug/CMakeFiles/hash_table.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hash_table.dir/ft.c.o"
+  "CMakeFiles/hash_table.dir/hash_table.c.o"
+  "CMakeFiles/hash_table.dir/hash_table_2.c.o"
+  "CMakeFiles/hash_table.dir/main.c.o"
+  "hash_table"
+  "hash_table.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hash_table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
