061b1a11452f5110c30600b024c83db8d06e2a9e
Pamelia Lettie <plettie@mi-g3.21-school.ru>
2020-02-09 19:22:05 +0300

    tests

---
+++ tests/if.sh
@@ -0,0 +1,34 @@
+if false; then
+  echo true;
+elif true; then
+  echo false;
+else
+  echo foo;
+fi
+
+if
+echo y
+then
+if
+echo a
+then
+if
+echo n
+then
+if
+echo e
+then
+if
+echo l
+then
+echo o
+if
+echo x
+then
+echo !
+fi
+fi
+fi
+fi
+fi
+fi
