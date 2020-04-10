8c2a75b6ed5bd4b74c225699e0d620efbf5cb6b3
Quickaxe-Martina <qmartina21@gmail.com>
2020-04-10 17:01:05 +0300

    fix_error_tab2

--- objs/nucleus/input_mode.d
+++
@@ -1,6 +0,0 @@
-objs/nucleus/input_mode.o: src/nucleus/input_mode.c \
- src/nucleus/../../inc/fshell.h src/nucleus/../../inc/../libft/libft.h \
- src/nucleus/../../inc/../libft/get_next_line.h \
- src/nucleus/../../inc/../libft/libft.h src/nucleus/../../inc/parser.h \
- src/nucleus/../../inc/struct.h src/nucleus/../../inc/nucleus.h \
- src/nucleus/../../inc/hash.h
