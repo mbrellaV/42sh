f2e2e509389537063f7549895dccee59437ee7f4
wstygg <wstygg@student.21-school.ru>
2020-05-12 17:42:56 +0800

    type

--- src/exec/ft_redirect_error.c
+++ src/exec/ft_redirect_error.c
@@ -6,7 +6,7 @@
 /*   By: wstygg <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 19:53:08 by wstygg            #+#    #+#             */
-/*   Updated: 2020/05/04 13:53:14 by wstygg           ###   ########.fr       */
+/*   Updated: 2020/05/12 17:28:00 by wstygg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	ft_redirect_error(int marker, char *dopline)
 	put_error_to_shell(2);
 	if (marker == 10)
 	{
-		ft_dprintf(globals()->fd[2], SHELL_NAME": %s: Bad file descriptor",
+		ft_dprintf(globals()->fd[2], SHELL_NAME": %s: Bad file descriptor\n",
 				dopline);
 		ft_strdel(&dopline);
 	}
 	if (marker == 9)
 	{
-		ft_dprintf(globals()->fd[2], SHELL_NAME": %s: ambiguous redirect",
+		ft_dprintf(globals()->fd[2], SHELL_NAME": %s: ambiguous redirect\n",
 				dopline);
 	}
 }
