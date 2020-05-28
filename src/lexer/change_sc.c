c040c301060efa5349f1809a8ec9fbacd38bf56f
mbrellaV <petyag2001@gmail.com>
2020-05-28 17:11:25 +0300

    +fixed {}

--- src/lexer/change_sc.c
+++ src/lexer/change_sc.c
@@ -44,7 +44,7 @@ void		ft_change_all_sc(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == '$' && str[i + 1] == '(')
+		if (str[i] == '$' && (str[i + 1] == '(' || str[i + 1] == '{'))
 			i += word_size(&str[i]);
 		if ((str[i] == '(' || str[i] == ')') && !check_if_in_par(str, i))
 			str[i] = ' ';
