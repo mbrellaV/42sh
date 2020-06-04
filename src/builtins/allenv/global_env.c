150180765c2fad02cc399458cf9a51d039490ae6
pankratdodo <alinapankratova@inbox.ru>
2020-06-04 21:01:47 +0500

    norm

--- src/builtins/allenv/global_env.c
+++ src/builtins/allenv/global_env.c
@@ -12,6 +12,22 @@
 
 #include "fshell.h"
 
+void	do_all_var(char **env)
+{
+	int		i;
+
+	i = -1;
+	if (!(globals()->g_all_var = (char **)ft_memalloc(sizeof(char *) *
+			(ft_env_len(env) + 2))))
+		ft_error_q(1);
+	while (env[++i])
+	{
+		if (!(globals()->g_all_var[i] = ft_strdup(env[i])))
+			ft_error_q(1);
+	}
+	env[i] = NULL;
+}
+
 void	ft_global_env(char **env, int argc)
 {
 	int		i;
